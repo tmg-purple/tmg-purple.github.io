960f7c6ab0452d041286fbb1a0e2fc4dec1e7bb0
tmg-purple <108309083+tmg-purple@users.noreply.github.com>
2022-06-28 14:53:48 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,135 @@
+module.exports = {
+    "mode": "jit",
+    "purge": [
+        ".\/**\/*.html"
+    ],
+    "darkMode": false,
+    "theme": {
+        "container": {
+            "center": true,
+            "padding": {
+                "DEFAULT": "1rem",
+                "sm": "2rem",
+                "lg": "4rem",
+                "xl": "5rem",
+                "2xl": "6rem"
+            }
+        },
+        "extend": {
+            "fontFamily": {
+                "title": [
+                    "Poppins"
+                ],
+                "body": [
+                    "Roboto"
+                ]
+            },
+            "borderRadius": {
+                "main": "0.5rem",
+                "button": "9999px"
+            },
+            "boxShadow": {
+                "main": "0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)"
+            },
+            "dropShadow": {
+                "main": [
+                    "0 10px 8px rgba(0, 0, 0, 0.04)",
+                    "0 4px 3px rgba(0, 0, 0, 0.1)"
+                ]
+            },
+            "backgroundImage": {
+                "body-image": "url('')"
+            },
+            "backgroundPosition": {
+                "body-position": "center"
+            },
+            "backgroundSize": {
+                "body-size": "cover"
+            },
+            "colors": {
+                "body": "#FFFAFA",
+                "header": "#FFFFFF",
+                "content": "#FFFFFF",
+                "primary": {
+                    "button": {
+                        "text": "#FFFFFF"
+                    },
+                    "DEFAULT": "#A855F7",
+                    "100": "#D0A3FA",
+                    "200": "#C894F9",
+                    "300": "#C085F9",
+                    "400": "#B875F8",
+                    "500": "#A855F7",
+                    "600": "#9736F5",
+                    "700": "#8F26F4",
+                    "800": "#8714F4",
+                    "900": "#7F0AED"
+                },
+                "secondary": {
+                    "button": {
+                        "text": "#A855F7"
+                    },
+                    "DEFAULT": "#FFFFFF",
+                    "100": "#FFFFFF",
+                    "200": "#FFFFFF",
+                    "300": "#FFFFFF",
+                    "400": "#FFFFFF",
+                    "500": "#FFFFFF",
+                    "600": "#E6E6E6",
+                    "700": "#D9D9D9",
+                    "800": "#CCCCCC",
+                    "900": "#C0C0C0"
+                },
+                "tertiary": {
+                    "DEFAULT": "#F97316",
+                    "100": "#FA9C58",
+                    "200": "#FA944A",
+                    "300": "#FA8A3D",
+                    "400": "#F98230",
+                    "500": "#F97316",
+                    "600": "#ED6606",
+                    "700": "#E05E05",
+                    "800": "#D35905",
+                    "900": "#C65405"
+                },
+                "cCC4ELuVTqT8Xg9FU3OM": {
+                    "link": "#FFFFFF",
+                    "link-hover": "#eeeeee",
+                    "mobile-link": "#FFFFFF",
+                    "mobile-link-hover": "#FFFFFF"
+                },
+                "yRIpI7TiM9FHOkdFRG3s": {
+                    "body": "#292524",
+                    "header": "#000000"
+                },
+                "i1Gr3QnsUNUTlCmqrAuR": {
+                    "body": "#292524",
+                    "header": "#000000"
+                },
+                "NbEmYwi6uJEpubUfUN4C": {
+                    "body": "#FFFAFA",
+                    "header": "#FFFAFA",
+                    "section-background": "#000000"
+                },
+                "0GHAh8y8": {
+                    "testimonial-background": "#1e293b"
+                },
+                "NQxWQzGxcMcycpDmlBEh": {
+                    "body": "#000000",
+                    "text": "#F1F1F1",
+                    "header": "#000000",
+                    "card-background": "#1e293b"
+                },
+                "HkWc46rD": {
+                    "section-background": "#000000"
+                },
+                "iViUV74kJHqqo7jXnERH": {
+                    "link": "#FFFFFF",
+                    "background": "#000000",
+                    "link-hover": "#eeeeee"
+                }
+            }
+        }
+    },
+    "plugins": []
+}
